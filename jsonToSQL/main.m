680be3ce6f045e023fcd8b1ea12f1506aca5862e
GiPyeong <lkp0907@gmail.com>
2015-01-03 15:20:35 +0900

    init Project

---
+++ jsonToSQL/main.m
@@ -0,0 +1,27 @@
+//
+//  main.m
+//  jsonToSQL
+//
+//  Created by GiPyeong Lee on 2015. 1. 3..
+//  Copyright (c) 2015년 com.devsfolder.jsonToSQL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        // 1. json 데이터 로드 객체화
+        
+        
+        
+        // 2. 키값을 통한 테이블 생성 SQL 문 작성
+        
+        // 3. 키-벨류 통한 데이터 삽입 SQL 문 작성
+        
+        
+
+    }
+    return 0;
+}
